5106e52b708e04a182df1a053490f4d6367f7ad0
Sandeepsowpati <33120390+sandeepsowpati@users.noreply.github.com>
2020-11-03 13:03:20 +0530

    Update unique_char.py

--- unique_char.py
+++ unique_char.py
@@ -1,3 +1,12 @@
-char_list = list(input())
-char_set = set(char_list)
-print(len(char_set))
+inp_str = str(input())
+res = [inp_str[i: j] for i in range(len(inp_str)) 
+          for j in range(i + 1, len(inp_str) + 1)]
+temp = []
+min_val = len(set(inp_str))
+for i in res:
+    if len(i) >= min_val and len(set(i))  == min_val:
+        temp.append(i)
+cou_ = len(inp_str)
+for i in temp:
+    cou_ = min(cou_,len(i))
+print(cou_)
